ab464f3ce0a0098791025ca068128691e01cac43
Nahuel <Alvareznahue@gmail.com>
2025-05-02 06:23:45 -0300

    ui:build

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const express = require('express');
-const morgan = require('morgan');
+const Person = require('./models/person')
+
 const app = express()
-const cors = require('cors')
+
+const morgan = require('morgan');
+const cors = require('cors');
 
 app.use(cors())
 app.use(express.json())
@@ -17,90 +21,97 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-let persons = [
-    { 
-      id: 1,
-      name: "Arto Hellas", 
-      number: "040-123456"
-    },
-    { 
-      id: 2,
-      name: "Ada Lovelace", 
-      number: "39-44-5323523"
-    },
-    { 
-      id: 3,
-      name: "Dan Abramov", 
-      number: "12-43-234345"
-    },
-    { 
-      id: 4,
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122"
-    }
-]
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
 
 app.get('/info', (request, response) => {
-  const totalPersons = `Phonebook has info for ${persons.length} people`
-  const date = new Date().toString()
+  Person.countDocuments({})
+    .then(count => {
+      const totalPersons = `Phonebook has info for ${count} people`
+      const date = new Date().toString()
 
-  response.send(`
-      <p>${totalPersons}</p>
-      <p>${date}</p>
-    `)
+      response.send(`
+          <p>${totalPersons}</p>
+          <p>${date}</p>
+        `)
+    })
+
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id) 
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  
-  response.status(204).end()
-}) 
+app.post('/api/persons', (request, response, next) => {
+  const { name, number } = request.body
 
-app.post('/api/persons', (request, response) => {
-  const id = Math.floor(Math.random() * 99999)
-  const body = request.body
-
-  if (!body.name || !body.number) {
+  if (!name || !number) {
     return response.status(400).json({
       error: 'content missing'
     })
   }
 
-  const nameExists = persons.some(person => person.name === body.name)
-  
-  if (nameExists) {
-    return response.status(400).json({
-      error: 'name must be unique'
+  const person = new Person({
+    name,
+    number
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
     })
-  }
-
- const person = {
-    id: id,
-    name: body.name,
-    number: body.number
-  }
-
-  persons = persons.concat(person)  
-  response.json(person)
+    .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  const person = {
+    name,
+    number
+  }
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    person,
+    { new: true }
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
